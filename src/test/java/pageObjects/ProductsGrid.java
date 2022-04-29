3ab1cfad89584ff8523f1c3dcc305ce2ba3fbcef
KatyaSh <katiasolo@tut.by>
2022-04-29 21:53:36 +0300

    Update ProductsGrid.java

--- src/test/java/pageObjects/ProductsGrid.java
+++ src/test/java/pageObjects/ProductsGrid.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.$$;
 public class ProductsGrid {
 
     public boolean checkProductTitleExist() {
-        ElementsCollection selenideElements = $$(By.xpath("//div[@class='catalog-navigation-list__aside-item catalog-navigation-list__aside-item_new catalog-navigation-list__aside-item_active']/div[@class='catalog-navigation-list__dropdown']/div[@class='catalog-navigation-list__dropdown-list']/a/span[@class='catalog-navigation-list__dropdown-data']/span[@class='catalog-navigation-list__dropdown-title']"));
+        ElementsCollection selenideElements = $$(By.xpath("//div[@class ='catalog-navigation-list__aside-item catalog-navigation-list__aside-item_active']/div/div/a/span/span[@class='catalog-navigation-list__dropdown-title']"));
         for (SelenideElement item : selenideElements) {
             if (!item.text().isEmpty()) {
                 return true;
@@ -19,7 +19,7 @@ public class ProductsGrid {
     }
 
     public boolean checkProductDescriptionExist() {
-        ElementsCollection selenideElements = $$(By.xpath("//div[@class='catalog-navigation-list__aside-item catalog-navigation-list__aside-item_new catalog-navigation-list__aside-item_active']/div[@class='catalog-navigation-list__dropdown']/div[@class='catalog-navigation-list__dropdown-list']/a/span[@class='catalog-navigation-list__dropdown-data']/span[@class='catalog-navigation-list__dropdown-description']"));
+        ElementsCollection selenideElements = $$(By.xpath("//div[@class ='catalog-navigation-list__aside-item catalog-navigation-list__aside-item_active']/div/div/a/span/span[@class='catalog-navigation-list__dropdown-description']"));
         for (SelenideElement item : selenideElements) {
             if (!item.text().isEmpty()) {
                 return true;
